d63904a32617cb66a1035537d7b4d37da4ab9318
Eden Goldfarb <goldie.ed@gmail.com>
2019-11-23 13:27:31 +0200

    moving

--- HW1.py
+++ HW1.py
@@ -1,18 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
+plt.rcParams['figure.figsize'] = (14.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
 
+# Read comma separated data
+df = pd.read_csv('C:\\Users\\goldi\\OneDrive\\Weizmann\\ML\\hw1\\kc_house_data.csv') # Relative paths are sometimes better than absolute paths.
+# df stands for dataframe, which is the default format for datasets in pandas
 
-
-
-
-#somasdasdasdasdkjalksdlkajsdkj
-
-
-
-
-
-
-
+df.head(n=10)
+df.describe
 
 
 
