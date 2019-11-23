82d31b0a954cb5463ddc0e6af999a62fca2c7688
Eden Goldfarb <goldie.ed@gmail.com>
2019-11-23 19:10:02 +0200

    Advancement

--- HW1.py
+++ HW1.py
@@ -47,13 +47,42 @@ ax.set_ylabel("Price")
 ax.set_title('Square feet VS Price')
 
 
+X = np.c_[np.ones(len(X)),X ]
+type(X)
+X.shape
 
+ def compute_cost(X, y, theta):
+    """
+    Computes the average squared difference between an obserbation's actual and
+    predicted values for linear regression.  
 
+    Input:
+    - X: Inputs  (n features over m instances).
+    - y: True labels (1 value over m instances).
+    - theta: The parameters (weights) of the model being learned.
 
+    Output:
+    - J: the cost associated with the current set of parameters (single number).
+    """
+    
+    J = 0  # Use J for the cost.
+    ###########################################################################
+    # TODO: Implement the MSE cost function.                                  #
+    ###########################################################################
+    m = X.shape[0]
+    n = X.shape[1]
+    J = (((theta.dot(X.transpose()) - y)**2).sum())/(2*m)
+        
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return J
 
+theta = np.array([2,1,1])
+z = np.arange(1,7,1).reshape(2,3).transpose()
+theta.dot(z)
 
-
-
+theta = np.array(np.random.random(size=X.shape[1]))
 
 
 
